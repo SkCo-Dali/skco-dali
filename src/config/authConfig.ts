e9045627f956df3a29f960de8840a52ab5a29fed
jostvian <jostvian@gmail.com>
2025-10-16 15:26:57 -0500

    Deleted authConfig.ts

--- src/config/authConfig.ts
+++
@@ -1,8 +0,0 @@
-
-import { ENV } from '@/config/environment';
-
-// Microsoft Graph API configuration
-export const graphConfig = {
-  graphMeEndpoint: `${ENV.GRAPH_API_BASE_URL}/me`,
-  graphPhotoEndpoint: `${ENV.GRAPH_API_BASE_URL}/me/photo/$value`
-};
